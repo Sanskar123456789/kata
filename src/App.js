66807267c0c48a7e133966fff71a933ab586ad5e
Sanskar Agarwal <sanskar.agarwal91@gmail.com>
2024-10-25 17:51:15 +0530

    Step 1 completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  const [sum, setSum] = React.useState(0);
+  const [numbers, setNumbers] = React.useState("");
+
+  React.useEffect(() => {
+    console.log(add("")); // Expected output: 0
+    console.log(add("1")); // Expected output: 1
+    console.log(add("1,5")); // Expected output: 6
+  }, []);
+
+  const add = (num) => {
+    if (num === "") return 0;
+
+    const numArray = num.split(",").map(Number);
+    return numArray.reduce((sum, num) => sum + num, 0);
+  };
+
   return (
-    <></>
+    <>
+      <input
+        type="text"
+        className="inp-text"
+        placeholder="Enter Text"
+        onChange={(e) => setNumbers(e.target.value)}
+      />
+      <button onClick={() => setSum(add(numbers))} className="btn">
+        Calculate
+      </button>
+      <div className="result">Result: {sum}</div>
+    </>
   );
 }
 
